4362426c0ff7bfb4e9a27bdbe34efa0b10d74805
pieter_blomme_home <pieter.blomme@gmail.com>
2019-12-22 21:29:31 +0100

    STATIC_ROOT

--- whatdidilearn/whatdidilearn/settings/base.py
+++ whatdidilearn/whatdidilearn/settings/base.py
@@ -120,7 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_ROOT = '/home/ubuntu/whatdidilearn/whatdidilearn/whatdidilearn/static'
 
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
