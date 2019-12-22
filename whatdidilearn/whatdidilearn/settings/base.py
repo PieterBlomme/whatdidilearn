1d7219e80834fe9a4236ed3a5a5f3374b89b7ae6
pieter_blomme_home <pieter.blomme@gmail.com>
2019-12-22 20:43:45 +0100

    static_root for production

--- whatdidilearn/whatdidilearn/settings/base.py
+++ whatdidilearn/whatdidilearn/settings/base.py
@@ -119,6 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
